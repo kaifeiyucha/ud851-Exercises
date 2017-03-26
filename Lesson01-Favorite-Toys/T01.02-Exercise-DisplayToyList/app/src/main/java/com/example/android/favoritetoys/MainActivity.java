bb86d22e381e06ac1f73e5ea379260b0cab84cfc
yuzhuol1 <liyuzhuoc_code@163.com>
2017-03-26 13:38:28 +0800

    second exercise for循环i

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -17,10 +17,12 @@ package com.example.android.favoritetoys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Declare a TextView variable called mToysListTextView
+    private TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +34,15 @@ public class MainActivity extends AppCompatActivity {
         // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
 
         // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        String toyNames[] = ToyBox.getToyNames();
+        for(int i=0; i<toyNames.length; i++){
+            mToysListTextView.append(toyNames[i]);
+            mToysListTextView.append("\n\n\n");
+
+        }
+
     }
+
 }
